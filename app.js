2a4cd0e23d5bab87d03cf1812b46557da24db528
Meister <meistercodes@gmail.com>
2022-03-06 00:07:09 -0500

    wordle

--- app.js
+++ app.js
@@ -1,69 +1,76 @@
-const tileDisplay = document.querySelector(".tile-container");
-const keyboard = document.querySelector(".key-container");
+const tileDisplay = document.querySelector('.tile-container');
+const keyboard = document.querySelector('.key-container');
 
+const wordle = 'SUPER';
 const keys = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  "ENTER",
-  "Z",
-  "X",
-  "C",
-  "V",
-  "B",
-  "N",
-  "M",
-  "BACK",
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  'BACK',
 ];
 
 const guessRows = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
 ];
 
-guessRows.forEach((guessRow, guessRowIndex) => {
-  const rowElement = document.createElement("div");
-  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
-  guessRow.forEach((guess, guessIndex) => {
-    const tileElement = document.createElement("div");
-    tileElement.setAttribute(
-      "id",
-      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
-    );
-    tileElement.classList.add("tile");
-    rowElement.append(tileElement);
-  });
-  tileDisplay.append(rowElement);
-});
+let currentRow = 0;
+let currentTile = 0;
 
-const handleClick = () => {
-  console.log("clicked");
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement('div')
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement('div')
+    tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+    tileElement.classList.add('tile')
+    rowElement.append(tileElement)
+  })
+  tileDisplay.append(rowElement);
+})
+
+keys.forEach(key => {
+  const buttonElement = document.createElement('button')
+  buttonElement.textContent = key
+  buttonElement.setAttribute('id', key)
+  buttonElement.addEventListener('click', () => handleClick(key))
+  keyboard.append(buttonElement)
+})
+
+const handleClick = (key) => {
+  console.log('clicked', key);
+  addLetter(key);
 };
 
-keys.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.textContent = key;
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", handleClick);
-  keyboard.append(buttonElement);
-});
+const addLetter = (letter) => {
+  const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+  tile.textContent = letter
+};
